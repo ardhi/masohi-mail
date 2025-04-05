9ebdfe3230c3b4aae5c3a0d6cdc5e1837eb511a5
Ardhi Lukianto <ardhi@lukianto.com>
2025-04-05 20:58:34 +0700

    Updates

--- plugin/factory.js
+++ plugin/factory.js
@@ -62,6 +62,18 @@ async function factory (pkgName) {
       if (!text) text = stripper(html)
       return { text, html }
     }
+
+    send = async (payload = {}) => {
+      const { find } = this.lib._
+      let { to, cc, bcc, from, subject, message, conn, options = {} } = payload
+      const c = find(this.connections, { name: conn })
+      if (!c) throw this.error('notFound%s%s', this.print.write('connection'), `${conn}@masohiMail`)
+      from = c.options.auth.user // can't change from if using smtp
+      const { text, html } = await this.formatMessage(message, options)
+      if (options.subject) subject = options.subject
+      const resp = await c.instance.sendMail({ from, to, cc, bcc, subject, text, html })
+      return resp
+    }
   }
 }
 
