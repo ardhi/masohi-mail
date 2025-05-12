314afc9244e543ae5fb1bdc87c7d5ec2a6cb7384
Ardhi Lukianto <ardhi@lukianto.com>
2025-05-12 22:56:34 +0700

    Updates

--- plugin/factory.js
+++ plugin/factory.js
@@ -67,6 +67,8 @@ async function factory (pkgName) {
       const { find } = this.lib._
       let { to, cc, bcc, from, subject, message, conn, options = {} } = payload
       const c = find(this.connections, { name: conn })
+      if (!c) return
+      // TODO: wrong layout...
       if (!c) throw this.error('notFound%s%s', this.print.write('connection'), `${conn}@masohiMail`)
       from = c.options.auth.user // can't change from if using smtp
       const { text, html } = await this.formatMessage(message, options)
